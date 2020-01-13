2dee84a2fda0972e924373ead67f9e82efa3a2ab
RuslanPr0g <59767834+RuslanPr0g@users.noreply.github.com>
2020-01-13 20:36:25 +0300

    v0.2

--- my-app/app.js
+++ my-app/app.js
@@ -2,27 +2,22 @@ const log = console.log;
 
 const request = require('request');
 
-const _units = 'units=si'
+const _geo = require('./geocode')
 
-let latitude = '46.47747'; let longitude = '30.73262'; // Odessa, Ukraine
+let data = {
+    latitude: '46.47747',  // Odessa, Ukraine
+    longitude: '30.73262',
+}
+
+let units = 'units=si'
 
 let the_adress = 'Odessa, Odesskaya oblast'; // = input('Your Country: ')
 
 const geocoding_url= 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + the_adress + '.json?access_token=pk.eyJ1IjoicnVzbGFucHJvZyIsImEiOiJjazU5cjlpNWowb2IyM2RyZnJqM2dyNzdqIn0.ozdJTwPL2kb8cLFHA53JTw&limit=1'
 
-request({ url: geocoding_url, json: true}, (error, response) => {
-    if(error)
-    {
-        log('There is some problem with connecting to the geocoding...')
-    }
-    else
-    {
-        latitude = response.body.features[0].center[1]
-        longitude = response.body.features[0].center[0]
-    }
-})
+_geo.to_geo(geocoding_url, data)
 
-const url = 'https://api.darksky.net/forecast/b97b17f7219eb126257cc47106b5147b/' + latitude + ',' + longitude + '?' + _units;
+const url = 'https://api.darksky.net/forecast/b97b17f7219eb126257cc47106b5147b/' + data.latitude + ',' + data.longitude + '?' + units;
 
 request({ url: url, json: true }, (error, response) => {
    
@@ -40,7 +35,7 @@ request({ url: url, json: true }, (error, response) => {
 
         const _temperature = response.body.currently.temperature
 
-        if(_units === 'units=si')
+        if(units === 'units=si')
         {
             if(_temperature <= 0.1){_desTemp = ' frozen water'}
             if(_temperature > 0.1 && _temperature < 10){_desTemp = ' enough cold'}
