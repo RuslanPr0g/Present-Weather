e06caba96546a0f50ea0e3b7dbe976070ed528ba
RuslanPr0g <59767834+RuslanPr0g@users.noreply.github.com>
2020-01-23 20:52:02 +0300

    v0.8.7(NOW-YOU-CAN-SEARCH-FOR-YOUR-CITY)

--- my-app/web-server/public/js/app.js
+++ my-app/web-server/public/js/app.js
@@ -1,8 +1,22 @@
-console.log('Client side js is loaded')
+console.log('Client side js is loaded.')
 
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
+const w_Form = document.querySelector('form');
+const search = document.querySelector('input');
+
+w_Form.addEventListener('submit', (event) => {
+
+    event.preventDefault() //stop refresh page
+
+    const location = search.value
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
     response.json().then((data) => {
-        console.log(data)
+        if (data.error){
+            console.log(data.error)
+        }else{
+           console.log(data.location)
+           console.log(data.forecast)
+        }
     })
 })
-
+})
