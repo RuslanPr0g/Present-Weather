94f9a166e47f3f8b8045f708d6a3cf959f24ab6c
RuslanPr0g <59767834+RuslanPr0g@users.noreply.github.com>
2020-06-04 23:27:13 +0300

    fix styles + features added

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,45 +1,36 @@
-console.log('Client side js is loaded.');
+console.log("Client side js is loaded.");
 
-const w_Form = document.querySelector('form');
-const search = document.querySelector('input');
-const message1 = document.querySelector('#_message1');
-const message2 = document.querySelector('#_message2');
+const w_Form = document.querySelector("form");
+const search = document.querySelector("input");
+const message1 = document.querySelector("#_message1");
+const message2 = document.querySelector("#_message2");
 
-//message1.textContent = 'Your Weather Right Now:'
-//message2.textContent = ''
-
-function viewDiv(){
+function viewDiv() {
   document.getElementById("container-content-id").style.display = "block";
-};
-function hideDiv(){
+}
+function hideDiv() {
   document.getElementById("container-content-id").style.display = "none";
-};
+}
 
-w_Form.addEventListener('submit', (event) => {
+w_Form.addEventListener("submit", (event) => {
   event.preventDefault(); //stop refresh page
 
   const location = search.value;
-  
 
-  fetch('/weather?address=' + location).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          console.log(data.error);
-          message2.textContent = data.error;
-          message1.textContent = ' ';
-          setTimeout(()=>
-          {
-            hideDiv();
-          }, 1500)
-        } else {
-          message1.textContent = data.location;
-          message2.textContent = data.forecast;
-          viewDiv();
-        }
-      });
-    }
-  );
-
+  fetch("/weather?address=" + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        console.log(data.error);
+        message2.textContent = data.error;
+        message1.textContent = " ";
+        setTimeout(() => {
+          hideDiv();
+        }, 1500);
+      } else {
+        message1.textContent = data.location;
+        message2.textContent = data.forecast;
+        viewDiv();
+      }
+    });
+  });
 });
-
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,101 +1,98 @@
-const log = console.log
+const log = console.log;
 
-const path = require('path')
-const hbs = require('hbs')
-const express = require("express")
-const main_path = path.join(__dirname, '../public')
-const app = express()
-const _geo = require('./append/geocode')
-const show_forecast = require('./append/show_forecast')
+const path = require("path");
+const hbs = require("hbs");
+const express = require("express");
+const main_path = path.join(__dirname, "../public");
+const app = express();
+const _geo = require("./append/geocode");
+const show_forecast = require("./append/show_forecast");
 
 //  Define paths for express config
 
 //const __host = (process.argv[2]); // chose the host
 const port = process.env.PORT || 3000;
-const viewsPath = path.join(__dirname, '../templates/views')
-const partialsPath = path.join(__dirname, '../templates/partials')
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 //  Setup handlebars engine and views location
-app.set('view engine', 'hbs')
-app.set('views', viewsPath)
-hbs.registerPartials(partialsPath)
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
 //  Setup static directory to serve
-app.use(express.static(main_path))
+app.use(express.static(main_path));
 
-app.get('/about', (req, res) => {
-    res.render('about', {
-        title: 'About Present Weather',
-        name: 'real time forecast'
-    })
-})
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Present Weather",
+    name: "real time forecast",
+  });
+});
 
-app.get('/help', (req, res) => {
-    res.render('help', {
-        title: 'FAQ',
-        name: 'Open all...'
-    })
-})
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "FAQ",
+    name: "Open all...",
+  });
+});
 
-app.get('', (req, res) => {
-    res.render('index', {
-        title: 'Real Time Weather',
-        name: 'Just type into it!'
-    })
-})
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Real Time Weather",
+    name: "Just type into it!",
+  });
+});
 
-app.get('/weather', (req, res) => {
-    if(!req.query.address){
-        return res.send({
-            error: 'There is no any address...'
-        })
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "There is no any address...",
+    });
+  }
+
+  _geo(req.query.address, (error, { latitude, longitude, location } = {}) => {
+    if (error) {
+      return res.send({ error });
     }
+    show_forecast(latitude, longitude, (error, fdata) => {
+      if (error) {
+        return res.send({ error });
+      }
+      res.send({
+        forecast: fdata,
+        location,
+        address: req.query.address,
+      });
+    });
+  });
+});
 
-    _geo(req.query.address, (error, { latitude, longitude, location }={}) => {
-        if(error){
-            return res.send({ error })
-        }
-        show_forecast(latitude, longitude, (error, fdata) => {
-            if(error){
-                return res.send({ error })
-            }
-            res.send({
-                forecast: fdata,
-                location,
-                address: req.query.address
-            })
-        })
-    })
-})
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: "Nothing to search...",
+    });
+  }
+  res.send({
+    products: [],
+  });
+});
 
-app.get('/products', (req, res) => {
-    if(!req.query.search){
-       return res.send({
-            error: 'Nothing to search...'
-        })
-    }
-    res.send({
-        products: [],
-    })
-})
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404 HELP",
+    error_404_message: "HELP PAGE NOT FOUND...",
+  });
+});
 
-app.get('/help/*', (req, res) => {
-    res.render('404', {
-        title: '404 HELP',
-        error_404_message: 'HELP PAGE NOT FOUND...'
-    })
-})
-
-app.get('*', (req, res) => {
-    res.render('404', {
-        title: "404",
-        error_404_message: '404'
-    })
-})
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    error_404_message: "404",
+  });
+});
 
 app.listen(port, () => {
-    log('Server is up on ' + port)
-})
-
-
-
+  log("Server is up on " + port);
+});
